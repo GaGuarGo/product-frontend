c99e332ae6908bc1a66a921b3957db5e6601cc09
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2023-08-29 09:55:40 -0300

    First Commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Product } from '../models/products';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+
+  products : Product[] = [];
+
+}
